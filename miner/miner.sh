462b1c3c74b0b1b6729da3517c78392d3a7f6ca3
kozyilmaz <kazim@monolytic.com>
2017-04-12 18:51:31 +0300

    miner script for restart daemon

---
+++ miner/miner.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# examples:
+# while true; do ./nheqminer-cpu -l zec.suprnova.cc:2142 -u WORKER_NAME -p WORKER_PASS -t 14; echo "sleep & restart"; sleep 30; done
+# while true; do ./nheqminer-cpu -l equihash.eu.nicehash.com:3357 -u BITCOIN_ADDRESS.worker1 -t 14; echo "sleep & restart"; sleep 30; done
+# while true; do ./nheqminer-gpu -l equihash.eu.nicehash.com:3357 -u BITCOIN_ADDRESS.worker1 -cd 0 -cb 16 -ct 128; echo "sleep & restart"; sleep 30; done
