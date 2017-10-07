cbd426045c85aa840c411dfcd8bed824b76132a8
kozyilmaz <kazim@monolytic.com>
2017-10-07 22:47:03 +0300

    README and miner script minor modifications

--- miner/miner.sh
+++ miner/miner.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-# examples:
-# while true; do ./nheqminer-cpu -l zec.suprnova.cc:2142 -u WORKER_NAME -p WORKER_PASS -t 14; echo "sleep & restart"; sleep 30; done
+# cpu
+# while true; do ./nheqminer-cpu -l eu1-zcash.flypool.org:3333 -u WALLET_ADDRESS.WORKER_NAME -t 4; echo "sleep & restart"; sleep 30; done
+# while true; do ./nheqminer-cpu -l zec.suprnova.cc:2142 -u WORKER_NAME -p WORKER_PASS -t 4; echo "sleep & restart"; sleep 30; done
 # while true; do ./nheqminer-cpu -l equihash.eu.nicehash.com:3357 -u BITCOIN_ADDRESS.worker1 -t 14; echo "sleep & restart"; sleep 30; done
-# while true; do ./nheqminer-gpu -l equihash.eu.nicehash.com:3357 -u BITCOIN_ADDRESS.worker1 -cd 0 -cb 16 -ct 128; echo "sleep & restart"; sleep 30; done
+
+# gpu
+# while true; do ./nheqminer-gpu -l eu1-zcash.flypool.org:3333 -u WALLET_ADDRESS.WORKER_NAME -cd 0; echo "sleep & restart"; sleep 30; done
